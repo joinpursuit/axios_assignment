d48158640347a7c0ee7686dad8f032a5d2b9c001
mateo-navarrete <mateonavarrete@pursuit.org>
2018-11-19 11:45:24 -0500

    added drawing cards solution

--- main.js
+++ main.js
@@ -1,11 +1,51 @@
 let $ = document;
-$.addEventListener('DOMContentLoaded', ()=> {
-  onLoad();
+let deck = {
+    id: undefined,
+    hand: [],
+  };
+(() => {
+  axios.get('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
+    .then(response => {
+      deck.id = response.data.deck_id;
+    })
+    .catch(err => {
+      console.log(err);
+    });
+})();
+
+$.addEventListener('DOMContentLoaded', () => {
+  loadDeck();
 });
 
-const onLoad = ()=> {
+const loadDeck = () => (new Promise(resolve => (deckLoading())));
+
+const deckLoading = () => (deck.id ? deckLoaded() : window.requestAnimationFrame(deckLoading));
+
+const deckLoaded = () => {
+  let h1 = $.querySelector('h1');
+  h1.innerText = 'Deck Loaded';
+  createAddButton();
+};
+
+const createAddButton = () => {
   let content = $.querySelector('.content');
-  let h1 = $.createElement('h1');
-  h1.innerText = 'Axios Card Shuffle';
-  content.appendChild(h1);
+  let button = $.createElement('button');
+  // button.value = 'Deal Cards';
+  button.innerText = 'Deal Cards';
+  content.appendChild(button);
+  button.addEventListener('click', (e)=> {
+    // e.preventDefault();
+    dealCards(5);
+  });
+};
+
+const dealCards = (n) => {
+  axios.get(`https://deckofcardsapi.com/api/deck/${deck.id}/draw/?count=${n}`)
+    .then(response => {
+      deck.hand = response.data.cards;
+      console.log(deck.hand);
+    })
+    .catch(err => {
+      console.log(err);
+    });
 };
