c28aba97871810f1696ddc64415b8a106ffe78e3
DianaG <dianagaona@pursuit.org>
2018-11-20 08:30:18 -0500

    Diana Gaona

--- test.js
+++ test.js
@@ -1,24 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-  let button = document.querySelector(".button");
-  function getId() {
+  function getId(deckId) {
     axios
-      .get("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
-      .then(res => {
-        return res.data.deck_id;
-      })
-      .then(deckId => {
-        axios
-          .get(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=5`)
-          .then(innerRes => {
-            innerRes.data.cards.forEach(card => {
-              let img = document.createElement("img");
-              img["src"] = card.image;
-              document.body.appendChild(img);
-              console.log(innerRes.data.cards);
-            });
-          });
+      .get(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=5`)
+      .then(innerRes => {
+        innerRes.data.cards.forEach(card => {
+          let img = document.createElement("img");
+          img["src"] = card.image;
+          document.body.appendChild(img);
+          console.log(innerRes.data.cards);
+        });
       });
   }
 
-  getId();
+  axios
+    .get("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
+    .then(res => {
+      return res.data.deck_id;
+    })
+
+    .then(deckId => {
+      let button = document.querySelector(".button");
+      button.addEventListener("click", () => {
+        getId(deckId);
+      });
+    });
 });
