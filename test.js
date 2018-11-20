96b6bc6e59e2581a2e24be373f9dd39cd1700e65
DianaG <dianagaona@pursuit.org>
2018-11-20 08:07:23 -0500

    Diana Gaona

--- test.js
+++ test.js
@@ -1,27 +1,8 @@
-// document.addEventListener("DOMContentLoaded", () => {
-//   function getId() {
-//     axios
-//       .get("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
-//       .then(res => {
-//         console.log(res);
-//         debugger;
-//         let deckId = res.data.deck_id;
-//         axios
-//           .get(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=5`)
-//           .then(innerRes => {
-//             console.log(innerRes, "this");
-//           })
-//           .catch(err => {
-//             console.log("ERROR ERROR ERROR ERROR");
-//           });
-//       });
-//   }
-// });
-
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
+  let button = document.querySelector(".button");
   function getId() {
     axios
-      .get('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
+      .get("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
       .then(res => {
         return res.data.deck_id;
       })
@@ -29,7 +10,10 @@ document.addEventListener('DOMContentLoaded', () => {
         axios
           .get(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=5`)
           .then(innerRes => {
-            innerRes.data.cards.forEach(cards => {
+            innerRes.data.cards.forEach(card => {
+              let img = document.createElement("img");
+              img["src"] = card.image;
+              document.body.appendChild(img);
               console.log(innerRes.data.cards);
             });
           });
@@ -38,29 +22,3 @@ document.addEventListener('DOMContentLoaded', () => {
 
   getId();
 });
-
-// function getCards() {
-//   axios
-//     .get('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
-//     .then(res => {
-//       showCard(res.data);
-//       debugger;
-//     })
-//     .catch(err => {
-//       debugger;
-//     });
-// }
-//
-// function showCard(arr) {
-//   let ul = document.querySelector('.ul');
-//   arr.forEach(card => {
-//     let li = document.createElement('li');
-//     li.innerText = ;
-//     ul.appendChild(li);
-//   });
-// }
-//
-// let button = document.querySelector('.button');
-// button.addEventListener('click', () => {
-//   getCards();
-// });
