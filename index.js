d1998128fcc9958302a20e1ac44777cce95b4eba
Jonathan.e <jonathanerquinigo@pursuit.org>
2018-11-20 14:16:08 -0500

    secondUpdate

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
   let deckId;
+  // let cardsImages;
   let button = document.querySelector(".button1");
+  let ptag = document.querySelector(".ptag");
   function getId() {
     axios
       .get("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
@@ -10,10 +12,6 @@ document.addEventListener("DOMContentLoaded", () => {
       });
   }
 
-  function drawCards = () => {
-
-  }
-
   getId();
 
   button.addEventListener("click", () => {
@@ -21,36 +19,23 @@ document.addEventListener("DOMContentLoaded", () => {
       .get(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=5`)
       // https://deckofcardsapi.com/api/deck/~deck_id~/draw/?count=5
       .then(innerResponse => {
-        console.log(innerResponse, "innerres");
+        let cards = innerResponse.data.cards;
+        if (!ptag.childNodes.length) {
+          cards.forEach(card => {
+            let cardsImages = card.image;
+            let image = document.createElement("img");
+            image.setAttribute("src", cardsImages);
+            ptag.appendChild(image);
+          });
+        } else {
+          ptag.removeChild(ptag.childNodes[0]);
+        }
+
+        // innerResponse.data.cards[0].images.png;
+        // console.log(innerResponse, "innerres");
       })
       .catch(err => {
         console.log(err, "this");
       });
   });
 });
-
-// response.data.deck_id
-
-// })
-
-/*document.addEventListener("DOMContentLoaded", () => {
-  let button = document.querySelector(".button")
-  button.addEventListener("click", fireRequest)
-
-    function fireRequest() {
-      let data = { title: 'Trying it out', body: "ljdsflkadsjklfjsadklfjkl" };
-
-      axios
-      .post("https://jsonplaceholder.typicode.com/posts", data)
-        .then(response => {
-          debugger
-          // return response.json();
-        })
-
-        .catch(err => {
-          debugger
-        })
-    }
-
-
-})*/
