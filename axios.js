ae939f49b8d3931825bc6be3649dbdf44e53c9e3
your name here <your_email_here>
2018-11-19 21:01:00 -0500

    almost done

--- axios.js
+++ axios.js
@@ -1,25 +1,62 @@
 document.addEventListener("DOMContentLoaded", () => {
-  //Load new deck
+  let button = document.querySelector(".button");
+  let select = document.querySelector(".handSize");
+  let hand = document.querySelector(".hand");
+  let draw = 1;
   //saved deck
   let deckid;
+
+  //Load new deck
+  function ssomething() {
+    for (let i = 1; i <= 10; i++) {
+      let option = document.createElement("OPTION");
+      option.innerText = i;
+      option.setAttribute("value", i);
+      select.appendChild(option);
+      console.log(select.value);
+    }
+  }
+  select.addEventListener("change", () => {
+    numofcards();
+  });
+  function numofcards() {
+    draw = select.value;
+  }
   function loadCards() {
     axios
       .get("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
       .then(res => {
+        //set deckid to res data
         deckid = res.data.deck_id;
-        console.log(deckid);
-        return res.data.deck_id;
-      })
-      .catch(err => {
-        debugger;
       });
+    // .catch(err => {});
   }
+  //invoke function to set deck id as soon as page loads
+
   loadCards();
-  let button = document.querySelector(".button");
-  button.addEventListener("click", drawCards());
+  ssomething();
+
+  button.addEventListener("click", () => {
+    drawCards();
+  });
 
   function drawCards() {
-    console.log(deckid);
+    let cards;
+    axios
+      .get(`https://deckofcardsapi.com/api/deck/${deckid}/draw/?count=${draw}`)
+      .then(res => {
+        cards = res.data.cards;
+
+        while (hand.firstChild) {
+          hand.removeChild(hand.firstChild);
+        }
+        cards.forEach(card => {
+          let img = document.createElement("IMG");
+          img.setAttribute("id", "acard");
+          img.src = card.image;
+          hand.appendChild(img);
+        });
+      });
   }
 });
 
