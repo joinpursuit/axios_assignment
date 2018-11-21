6605ed4a51da9a5aea0b912c2f6d1e25e065d65e
Pixelynx <tjstevens1992@gmail.com>
2018-11-21 18:31:37 -0500

    finally got cards

--- script.js
+++ script.js
@@ -2,35 +2,38 @@ document.addEventListener("DOMContentLoaded", () => {
   let button = document.querySelector(".button");
   let body = document.querySelector('body');
 
-  let deck = {
-    id: '',
-    cards: [],
-    remaining: 52
-  }
-
   // const discard_id;
   // const shuffleDeck = `https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`;
   // const reshuffleDeck = `https://deckofcardsapi.com/api/deck/${deck_id}/shuffle/`;
   // const discard = `https://deckofcardsapi.com/api/deck/${discard_id}/pile/<<pile_name>>/add/?cards=${discard_cards}`;
 
 const getHand = (deck_id) => {
-  axios
+    axios
     .get(`https://deckofcardsapi.com/api/deck/${deck_id}/draw/?count=5`)
-      .then(deck_id => {
-        let img = document.createElement('img');
-        img['src'] = deck_id['image'];
-        body.appendChild(img);
-      })
+      .then(response => {
+        let deck = response['data']['cards'];
+        deck.forEach(card => {
+            let img = document.createElement('img');
+            if (img !== null) {
+            img.src = card['image'];
+            body.appendChild(img);
+        } else {
+          img.remove();
+        }
+        })
+
+    })
 }
 
 axios
   .get(`https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`)
     .then(response => {
       return response['data']['deck_id']
-    }).then(deck_ID => {
+    }).then(deck_id => {
+
       button.addEventListener('click', () => {
-        getHand(deck_ID);
-        
+        getHand(deck_id);
+
       })
     })
 
