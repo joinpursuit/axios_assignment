ec4646b62fbb19c22611dee9bcb249a40c1908ec
Morteza Khaki <mortezakhaki@Mortezas-MacBook-Air.local>
2018-11-21 21:33:15 -0500

    Done

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // let myP = document.querySelector(".myP");
   let deck_id;
   axios
     .get("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
-
     .then(res => {
       deck_id = res.data.deck_id;
     })
     .catch(err => {
       debugger;
     });
+
+  let select = document.querySelector(".numCards");
+  let selectNum = 1;
+
+  select.addEventListener("change", event => {
+    selectNum = parseInt(event.target.value);
+    console.log(selectNum);
+  });
   let button = document.querySelector(".button");
   button.addEventListener("click", event => {
     axios
-      .get(`https://deckofcardsapi.com/api/deck/${deck_id}/draw/?count=5`)
+      .get(
+        `https://deckofcardsapi.com/api/deck/${deck_id}/draw/?count=${selectNum}`
+      )
       .then(res => {
         let allImages = document.querySelector(".cards");
         while (allImages.children.length) {
@@ -21,15 +29,10 @@ document.addEventListener("DOMContentLoaded", () => {
         }
         console.log(allImages);
         console.log(res.data.cards);
-
         res.data.cards.forEach(card => {
           let image = document.createElement("img");
           image.src = card.image;
-          // if (allImages.length === 0) {
           allImages.appendChild(image);
-          // } else {
-          //   image.removeChild(image.childNodes[0]);
-          // }
         });
       });
 
