03fb1e6c85d3299f29a0c7a1c9c74042482136f3
Nielene Thomas <nielenethomas@pursuit.org>
2018-11-26 21:42:58 -0500

    3rd commit

--- index.js
+++ index.js
@@ -1,54 +1,60 @@
+let deckId = '';
 document.addEventListener("DOMContentLoaded", () => {
 
 console.log("test");
   let button = document.querySelector(".button");
-
-  button.addEventListener("click", fireRequest)
+  button.addEventListener("click", reFireRequest())
 
 
   function fireRequest() {
-
     axios
     .get("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1" //, deck_count//
   )
     .then(res => {
+      deckId = res.data.deck_id;
+      // showDeck(res.data.deck_id)
 
-      showDeck(res.data.deck_id)
-
-let info = `${res.data.deck_id}`
-
-console.log(res, info);
-console.log(info);
-console.log("id:",res.data.deck_id);
-      // debugger
-      // return res.data.deck_id
+      // let info = res.data.deck_id
+      console.log("RES:::",res, "ID:::",info, "ID:",res.data.deck_id);
     })
     .catch(err => {
       console.log("error: ", err);
-      // debugger
-      // error()
     })
-
   }
 
-function showDeck(res) {
-  let display = document.querySelector(".display");
-  // arr.forEach(movie => {
 
-  // display.innerHTML = res.deck_id;
 
-  // console.log("here", typeof res.deck_id);
 
-  display.innerHTML = res;
-  // debugger
-
-  // ul.appendChild("li")
-  // })
+function reFireRequest() {
+  axios
+  .get(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=5` //, deck_count//
+)
+  .then(res => {
+    let fiveCards = res;
+    // showDeck(res.data.deck_id)
+// debugger
+    // let info = `${res.data.deck_id}`
+    console.log(res, info);
+    console.log("id:",res.data.deck_id);
+  })
+  .catch(err => {
+    console.log("error: ", err);
+  })
 }
 
 
 
+function showDeck(res) {
+  let display = document.querySelector(".display");
+  display.innerHTML = res;
 
+  // if (!res.length) {
+    // fireRequest()
+  // } else {
+    // reFireRequest()
+  // }
+
+}
 
 
 
