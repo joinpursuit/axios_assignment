4995657565f7fb2e28d5b6e594053e2aa732e7a7
crystal cardona <crystalcardona@pursuit.org>
2019-12-03 02:09:43 -0500

    still working

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const fecthData = (url, callback) => {
+    
+    let div = document.createElement("div")
+    div.id = "cards"
+
+    const fetchData = (url, callback) => {
         fetch(url)
         .then(res => {
-            if(res.notok === false){
+            if(!res.ok){
                 throw Error(res.statusText)
             }
             return res.json()
@@ -13,12 +17,37 @@ document.addEventListener("DOMContentLoaded", () => {
         .catch(err => {
             console.log(err)
         })
-        const drawCards = (data) => {
-            let output = []
-            deckId = 
-
-        }
-
-        fetchData("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`")
+    
     }
+    
+    const deckId = (data) => {
+        let p = document.createElement("p")
+        p.innerText = data.deck_id
+    }
+    const drawCard = (data) => {
+        let div = document.querySelector("#cards")
+        let cards = data.cards
+        cards.forEach(card => {
+            let li = document.createElement("li")
+            let img = document.createElement("img")
+            img.src = card.image
+            li.appendChild(img)
+            ol.appendChild(li)
+        })
+        div.appendChild(ol)
+    }
+    fetchData("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1", deckId)
+    let select = document.querySelector("select")
+    for(let i = 0; i <= 10; i++){
+        let option = document.createElement("option")
+        option.innerText = i
+        select.appendChild(option)
+    }
+    
+    let button = document.querySelector("button")
+    button.addEventListener("click", () => {
+        let deck = document.querySelector("p")
+        let numCards = Number(document.querySelector("#select").value)
+            fetchData(`https://deckofcardsapi.com/api/deck/${deck}/draw/?count=${numCards}`, drawCard)
+        })
 }) 
