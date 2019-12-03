fba030b5ae5b008eb857869f716e8460ca4bb53b
DejaLFlynn <dejaflynn@pursuit.org>
2019-12-02 22:07:03 -0500

    start

--- drawCard.js
+++ drawCard.js
@@ -7,23 +7,31 @@ document.addEventListener("DOMContentLoaded",() => {
                 throw Error(res.statusText);
             }
             return res.json();
-        }) .then(res => {
+        }).then(res => {
             callback(res)
             debugger
-        }) .catch(err => {
+        }).catch(err => {
             console.log(err)
         })
     }
     const createDeck = (data) => {
         
-        let deck = document.createElement('p')
+        let deck = document.querySelector('p')
         deck.innerText = data.deck_id   
     }
-    let button = document.createElement("button");
-    button.addEventListener("click", () => {
-        let url = "https://deckofcardsapi.com/api/deck/~deck_id~/draw/?count=5"
-    })
+    draw(){
+        
+    }
+   
     
     fecthData("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1/", createDeck)
+    let button = document.querySelector("button");
+    button.addEventListener("click", () => {
+        let url = "https://deckofcardsapi.com/api/deck/~deck_id~/draw/?count=5"
+        let deck = document.querySelector("p").innerText
+        let counter = Number(document.querySelector("#selectOption").value)
+        fecthData(`https://deckofcardsapi.com/api/deck/${deck}/draw/?count=${counter}`)
+        draw()
+    })
     let url = "https://deckofcardsapi.com/api/deck/new/"
 })
