d3a5682e0808a736be06306fa07341fbb6d14308
Kelvin <kelvinarellano@pursuit.org>
2019-12-02 21:35:55 -0500

    second issue

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 document.addEventListener("DOMContentLoaded", ()=>{
+    let deckId = "";
     fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1").then(res=>{
         return res.json()
     }).then(res=>{
-        let deckId = "";
         deckId = res["deck_id"]
-           debugger
         })
-    
+        
+    fetch(`https://deckofcardsapi.com/api/deck/` + deckId + `/draw/?count=5`).then(res=>{
+        console.log(res)
+        debugger
+    })
 })
