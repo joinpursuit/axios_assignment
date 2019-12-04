43769ddd245d6aecd49f942785373c73db1f1caa
Kevin Wong <kevinwong@pursuit.org>
2019-12-03 21:25:16 -0500

    successfully change the button event to display cards using async / await

--- fetch_lab.js
+++ fetch_lab.js
@@ -6,34 +6,35 @@ document.addEventListener("DOMContentLoaded", () => {
             let res = await axios.get("https://deckofcardsapi.com/api/deck/new/") 
             deck_id = res.data.deck_id;
             let shuffled = await axios.get(`https://deckofcardsapi.com/api/deck/${deck_id}/shuffle/`)
-            debugger
         } catch (err) {
             console.log(err);
         }
     }
 
-    const drawFive = (deck_id) => {
-        axios.get(`https://deckofcardsapi.com/api/deck/${deck_id}/draw/?count=5`)
-        debugger
-        
-        let deck = document.querySelector("#five")
-        deck.innerHTML = "";
-        for(let i = 0; i < data.cards.length; i++){
-            let img = document.createElement("img");
-            let src = data["cards"][i]["image"];
-            img.src = src;
-            deck.appendChild(img);
+    const drawFive = async (id) => {
+        try {
+            let drawCards =  await axios.get(`https://deckofcardsapi.com/api/deck/${id}/draw/?count=5`)
+            debugger
+            let deck = document.querySelector("#five")
+            deck.innerHTML = "";
+            for(let i = 0; i < drawCards.data.cards.length; i++){
+                let img = document.createElement("img");
+                let src = drawCards.data["cards"][i]["image"];
+                img.src = src;
+                deck.appendChild(img);
+            }
+        } catch (err){
+            console.log(err);
         }
+        
     }
 
 
     let button = document.querySelector("#drawBtn");
     button.addEventListener("click", () => {
-        let url = "https://deckofcardsapi.com/api/deck/" + id + "/draw/?count=5";
-        drawFive();
+        drawFive(deck_id);
     })
     
     fetchData();
-    drawFive()
     
 })
