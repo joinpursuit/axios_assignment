999dce91c69fa4222f6bdb6384ca4c0bb3341af5
your name here <your_email_here>
2019-12-03 19:29:05 -0500

    adding shuffle

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }).then(res => {
         let id = document.querySelector("#deckID")
         id.innerText = res.deck_id;
+        fetch("https://deckofcardsapi.com/api/deck/"+id.innerText+"/shuffle/")
     }).catch(err => {
         debugger;
     })
@@ -16,18 +17,21 @@ document.addEventListener("DOMContentLoaded", () => {
     button.addEventListener("click", () => {
         let id = document.querySelector("#deckID");
         let e = id.innerText;
-        let url = "https://deckofcardsapi.com/api/deck/" + e +"/draw/?count=5"
-        console.log(url)
+        let url = "https://deckofcardsapi.com/api/deck/" + e +"/draw/?count=5";
+        // let url = "https://deckofcardsapi.com/api/deck/"+ e +"/shuffle/";
         fetch(url).then(res => {
             if(!res.ok) {
                 throw Error(res.statusText);
             }
+            // debugger;
             return res.json();
         }).then(res => {
             let deck = document.querySelector("#deck");
             res.cards.forEach(el => {
+                
                 let image = document.createElement("img");
                 image.src = el.image;
+
                 deck.appendChild(image);
             })
             // let imageURL = res.cards[0].image;
