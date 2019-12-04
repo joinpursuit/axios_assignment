4c7196e99414a7986a7a1c4ab01920d85b002775
Deja Flynn <dejaflynn@Flynns-iMac.local>
2019-12-04 10:00:39 -0500

    final

--- index.js
+++ index.js
@@ -1,21 +1,12 @@
-
-
-// const axios = require("axios");
-
 document.addEventListener("DOMContentLoaded", () => {
-    // debugger;
+ 
     axios.get("https://deckofcardsapi.com/api/deck/new/").then(res => {
-        // debugger;
-    //     if(!res.ok) {
-    //         throw Error(res.statusText);
-    //     }
-    //     return res.json();
-    // }).then(res => {
+
         let id = document.querySelector("#deckID");
-        // id.innerText = res.deck_id;
+
         id.innerText = res.data.deck_id;
         axios.get("https://deckofcardsapi.com/api/deck/"+id.innerText+"/shuffle/")
-        // fetch("https://deckofcardsapi.com/api/deck/"+id.innerText+"/shuffle/")
+     
     }).catch(err => {
         debugger;
     })
@@ -26,14 +17,9 @@ document.addEventListener("DOMContentLoaded", () => {
         let id = document.querySelector("#deckID");
         let e = id.innerText;
         let url = "https://deckofcardsapi.com/api/deck/" + e +"/draw/?count=2";
-        // let url = "https://deckofcardsapi.com/api/deck/"+ e +"/shuffle/";
+       
         axios.get(url).then(res => {
-            // debugger;
-            // if(!res.ok) {
-            //     throw Error(res.statusText);
-            // }
-            // // debugger;
-            // return res.json();
+
             let deck = document.querySelector("#deck");
             res.data.cards.forEach(el => {
                 let image = document.createElement("img");
@@ -42,8 +28,7 @@ document.addEventListener("DOMContentLoaded", () => {
             })
             
         })
-            // let imageURL = res.cards[0].image;
-            // console.log(imageURL)
+
         .catch(err => {
             debugger
         })
