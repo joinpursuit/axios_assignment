7da5caf12ba0c1334a744808df11db1be2f6d44c
Jovanni <jovanniluna@pursuit>
2019-12-03 22:04:06 -0500

    Completed until selector

--- cards.js
+++ cards.js
@@ -1,45 +1,42 @@
 document.addEventListener("DOMContentLoaded" , () =>{
-    
-    const fetchData = (url,callback) =>{
-        fetch(url).then(res=>{
-            if(!res.ok){
-                throw Error(res.statusText)
-            }
-            return res.json();
-        }).then(res =>{
-            callback(res)
-        }).catch(err =>{
-            console.log(err)
-        })
-    }
-
-    let deckId 
-
-    const firstDeck = () =>{
-        let url ="https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1"
-        fetchData(url,(data)=>{
-            let p = document.createElement("p")
-            p.innerText = data.deck_id
+    let button = document.querySelector("button")
+    const buildDeck = async () =>{
+        let display = document.querySelector("#display")
+        display.innerHTML=""
+        try {
+            let deck = await axios.get(`https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`);
+            let deck_id = deck.data.deck_id
+            let p=document.createElement("p")
+             p.innerText = deck_id
+             p.style.display= "none"
             document.body.appendChild(p)
-        })
+        } catch(err) {
+            console.log(err);
+        }
     }
+    buildDeck()
 
-    firstDeck()
-
-    
-    const buildDeck = (data) =>{
-        debugger
+    const drawNewCards = async () =>{
+        let display = document.querySelector("#display")
+        display.innerHTML=""
+        try {
+            let p=document.querySelector("p")
+            let deck_id = p.innerText
+            let numCards = 1
+            let select = document.querySelector("select");
+            select.addEventListener("change", (e) =>{
+                return  numCards = e.currentTarget.value;
+            })
+            let drawCards = await axios.get(`https://deckofcardsapi.com/api/deck/${deck_id}/draw/?count=${numCards}`)
+            let cardArray = drawCards.data.cards
+            cardArray.forEach(card =>{
+                let image = document.createElement("img")
+                image.src = card.image
+                display.appendChild(image)
+            })
+        } catch(err) {
+            console.log(err);
+        }
     }
-
-    const drawCard = () =>{
-        let deck = document.querySelector("p")
-        let deckId = deck.innerHTML
-        let url = "https://deckofcardsapi.com/api/deck/"+deckId+"/draw/?count=5"
-        fetch (url)
-        console.log(url)
-    }
-
-    drawCard()
-
-    
+    button.addEventListener("click", drawNewCards)
 })
