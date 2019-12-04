ed02786a7f8d82a04f502fa4816e82e4f7e47e67
crystal cardona <crystalcardona@pursuit.org>
2019-12-04 03:01:14 -0500

    working on CSS next

--- axios.js
+++ axios.js
@@ -1,30 +1,50 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const fetchDeckofCards = async () => {
-        try{
-            let res = await axios.get("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
-            let deckId = res.data.deck_id
-        }catch(err){
-            console.log("ERROROROROROR")
-        }
-    }
-    const showFiveCards = async (data) => {
-
-        try{
-            let gotFive = await axios.get(`https://deckofcardsapi.com/api/deck/${data}/draw/?count=5`)
-            let cards = document.querySelector("#cards")
-            for(let i = 0; gotFive.data.cards.length; i++){
-                let img = document.createElement("img")
-                img.src = src
-                cards.appendChild(img)
-            }
-        }catch(err){
+document.addEventListener("DOMContentLoaded", () =>{
+    let deck_id;
+    const fetchAllCards = async () =>{
+      try{
+        let res = await axios.get("https://deckofcardsapi.com/api/deck/new/");
+        deck_id = res.data.deck_id
+        let shuffle = await axios.get(`https://deckofcardsapi.com/api/deck/${deck_id}/shuffle/`);
+      } 
+        catch(err) {
             console.log(err)
         }
     }
-    let button = document.querySelector("button")
-    button.addEventListener("click", () =>{
-        showFiveCards()
-    })
-    fetchDeckofCards()
-})
+ 
+    
+    let select = document.querySelector("select")
+    for(let i = 1; i <= 10; i++){
+        let option = document.createElement("option")
+        option.innerText = i
+        select.appendChild(option)
+    }
 
+   const showCards = async (deck_id) => {
+       try{
+        let image = document.querySelector("img")
+        if(image){
+            image.parentNode.removeChild(image)
+        }
+        let select = document.querySelector("select")
+        let numCards = select.value
+        let drawCards = await axios.get(`https://deckofcardsapi.com/api/deck/${deck_id}/draw/?count=${numCards}`);
+        let deck = document.querySelector("#cards")
+        for(let i =0; i < drawCards.data.cards.length; i++){
+            let img = document.createElement("img")
+            let src = drawCards.data["cards"][i]["image"]
+            img.src = src
+            deck.appendChild(img)
+       } 
+     } 
+     catch(err) {
+        console.log(err)
+    }
+   }
+
+   let button = document.querySelector("button")
+   button.addEventListener("click", ()=> {
+        showCards(deck_id)
+   })
+
+   fetchAllCards()
+})
