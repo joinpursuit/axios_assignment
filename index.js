1b5b8289b9077da9568944eb8d65c159e5a349ad
Nilber Remon <nilberremon@gmail.com>
2019-12-04 15:44:58 -0500

    Axios Module installation

--- index.js
+++ index.js
@@ -25,3 +25,27 @@ document.addEventListener("DOMContentLoaded", () => {
 
 })
 
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    let deckId
+    const fetchData = (url, callback) => {
+        fetch(url).then(res => {
+            if (!res.ok) {
+                throw Error(res.statusText);
+            }
+            return res.json();
+        }).then(json => {
+            callback(json);
+        }).catch(err => {
+            console.log(err);
+        })
+    }
+    const grabId = (data) => {
+        let deckId = data.deck_id
+        return deckId;
+    }
+    fetchData("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1", grabId)
+    
+    
+})
