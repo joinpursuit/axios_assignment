75c903c0093e9e4ce050749a00ea049a5878def7
bsihame <sihamebazi@pursuit.org>
2019-12-03 20:59:09 -0500

    add number draw fetch

--- axios_lab.js
+++ axios_lab.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", () =>{
+    
     const fetchData = (url, callback) => {
         fetch(url).then(res => {
             if(!res.ok){
@@ -14,30 +15,32 @@ document.addEventListener("DOMContentLoaded", () =>{
     }
 
     const getId = (data) => {
-        let p = document.querySelector("#id")
-        p.innerText = data.deck_id
+        firstId = data.deck_id
     }
 
     fetchData("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1", getId)
     let button = document.querySelector("#btm")
-    button.addEventListener("click",)
+    button.addEventListener("click", getId)
     let div = document.querySelector("shuffleDeck")
-    let ul = document.createElement("ul")
-    div.appendChild(ul)
-    let li = document.createElement("li")
-    ul.appendChild(li)
-    const drawCards = (data) => {
-        let output = []
-        let cards = data.deck_id
-        for(let key in cards) {
-            cards
-        }
+   
+    const drawCards = (firstId) => {
+        let numCards = 0;
+        let p = document.querySelector("#getCard")
+        numCards = event.target.value
+        p.innerText(numCards)
     }
 
+        fetchData("https://deckofcardsapi.com/api/deck/${firstId}/draw/?count=`$numCards`", drawCards)
+        let select = document.querySelector("select")
+        select.addEventListener("change", (event) => {
+
+    })
+
+
     
-
-
-
-
-
 })
+
+
+
+
+
