6811962701f63af3ba12f38d865e8f12688a2be1
BrandonBrown <brandonbrown@pursuit.org>
2019-12-03 21:05:13 -0500

    save

---
+++ index.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () =>{
+    let savedId = ""
+
+    const fetchData = async () => {
+        let shuffledDeck = await axios.get(`https://deckofcardsapi.com/api/deck/${deckId}/shuffle/`)
+        let deckId = res.data.deck_id
+
+        let 
+
+    }
+   
+   
+})
